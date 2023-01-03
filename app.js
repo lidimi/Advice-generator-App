3136b20986f214c7f1e1bcb0a6b2bee640edfb69
lidimi <114875769+lidimi@users.noreply.github.com>
2023-01-03 01:41:08 +0100

    Update app.js
    remove console logs

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ async function getAdvice() {
   let obj;
   const response = await fetch("https://api.adviceslip.com/advice");
   obj = await response.json();
-  console.log(obj);
   document.querySelector(".advice-id").innerHTML = obj.slip.id;
   document.querySelector(".quotation").innerHTML = obj.slip.advice;
 }
